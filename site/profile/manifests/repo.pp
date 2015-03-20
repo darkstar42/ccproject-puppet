f481e7b04582c53be88d76c759f001cf50afbf64
Michael Haase <darkstar@gernox.de>
2015-03-20 19:08:45 +0100

    Disable src for deployment repository

--- site/profile/manifests/repo.pp
+++ site/profile/manifests/repo.pp
@@ -1,9 +1,10 @@
 class profile::repo {
   apt::source { 'deployment':
-    location   => 'http://deploy.cc.gernox.de/',
-    release    => 'trusty',
-    repos      => 'all',
-    key        => '2B95C5A0',
-    key_source => 'http://deploy.cc.gernox.de/deployment.gpg'
+    location          => 'http://deploy.cc.gernox.de/',
+    release           => 'trusty',
+    repos             => 'main',
+    key               => '2B95C5A0',
+    key_source        => 'http://deploy.cc.gernox.de/deployment.gpg',
+    include_src       => false
   }
 }
