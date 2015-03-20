abbe23fcac2ded22d795af1e0d1afc3c6818acbb
Michael Haase <darkstar@gernox.de>
2015-03-20 18:56:28 +0100

    Update key id for repository

--- site/profile/manifests/repo.pp
+++ site/profile/manifests/repo.pp
@@ -3,7 +3,7 @@ class profile::repo {
     location   => 'http://deploy.cc.gernox.de/',
     release    => 'trusty',
     repos      => 'all',
-    key        => '5A281CD4',
+    key        => '2B95C5A0',
     key_source => 'http://deploy.cc.gernox.de/deployment.gpg'
   }
 }
