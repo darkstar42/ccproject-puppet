49387816728cb6d3f651e8fce57a21a6f9c07d70
Michael Haase <darkstar@gernox.de>
2015-03-20 11:25:43 +0100

    Add module golja/gnupg

---
+++ site/profile/manifests/gnupg.pp
@@ -0,0 +1,3 @@
+class profile::gnupg {
+  include ::gnupg
+}
--- site/role/manifests/puppet/master.pp
+++ site/role/manifests/puppet/master.pp
@@ -1,5 +1,6 @@
 class role::puppet::master {
   include profile::base
+  include profile::gnupg
   include profile::puppet::master
   include profile::reprepro
 }
