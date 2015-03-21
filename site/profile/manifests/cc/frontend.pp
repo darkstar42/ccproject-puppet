3d43ea8c91f6ebef5538d0ec93400f981cbdbe7f
Michael Haase <darkstar@gernox.de>
2015-03-21 21:33:04 +0100

    Add AllowOverride All

--- site/profile/manifests/cc/frontend.pp
+++ site/profile/manifests/cc/frontend.pp
@@ -9,6 +9,11 @@ class profile::cc::frontend {
     port            => '80',
     docroot         => '/opt/ccproject-frontend',
     manage_docroot  => false,
+    directories     => [
+      { path           => '/opt/ccproject-frontend',
+        allow_override => ['All'],
+      },
+    ],
     require         => Package["ccproject-frontend"],
   }
 }
